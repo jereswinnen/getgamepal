9e32421d0aa8d3fad4f5a5ec23af475054681b4c
Jeremy Swinnen <hey@jeremys.be>
2026-02-09 09:37:47 +0100

    Removed commented-out code from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "https",
-//         hostname: "images.igdb.com",
-//         pathname: "/**",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
